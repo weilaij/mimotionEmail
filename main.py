823d0de2a1c15761ab023018aece04edfffb14cf
wljiang <weilaij@outlook.com>
2024-09-04 07:26:44 +0800

    updat

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import time
 import requests
 
 # 开启根据地区天气情况降低步数（默认关闭）
-open_get_weather = sys.argv[3]
+# open_get_weather = sys.argv[3]
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = sys.argv[4]
+# area = sys.argv[4]
 
 # 以下如果看不懂直接默认就行只需改上面
 
@@ -237,4 +237,13 @@ def get_app_token(login_token):
 
 
 if __name__ == "__main__":
-    getBeijinTime()
+    user_mi = sys.argv[1]
+    min_1 = 100
+    max_1 = 300
+    # 登录密码
+    passwd_mi = sys.argv[2]
+    user_list = user_mi.split('#')
+    passwd_list = passwd_mi.split('#')
+    if len(user_list) == len(passwd_list):
+        for user_mi, passwd_mi in zip(user_list, passwd_list):
+            main(user_mi, passwd_mi, min_1, max_1)
